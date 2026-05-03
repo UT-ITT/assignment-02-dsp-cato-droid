7e828578fd4f2414369c8452846215f3ba7a63fe
Cato Kurtz <cato.kurtz@student.uni-tuebingen.de>
2026-05-03 22:03:45 +0200

    added mid note to freq conversion

--- read_midi/read_midi.py
+++ read_midi/read_midi.py
@@ -1,5 +1,11 @@
 import mido
 from mido import MidiFile
 
-for msg in MidiFile('berge.mid').play():
-    print(msg)
+for msg in MidiFile('Alle_Meine_Entchen.mid').play():
+    #print(msg)
+    if msg.type == "note_on":
+        print(f"note: {msg.note}, frequency: {440 * 2 **((msg.note - 69) / 12)}")
+
+#Note Frequency = 440Hz * 2^( (note - 69) / 12 )
+#https://www.phys.unsw.edu.au/jw/notes.html
+#https://www.reddit.com/r/embedded/comments/exxrub/popular_tunes_as_list_of_notes_frequencies/
